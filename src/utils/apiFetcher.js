a4b543d9c5f93f6d2c163f67b046bc6a06c2168c
EGOR <56518726+EgorMaloedov@users.noreply.github.com>
2024-02-19 22:46:47 +0300

    Delete src directory

--- src/utils/apiFetcher.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchDataFromAPI(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Ошибка при получении данных из API:', error);
-    }
-    }
